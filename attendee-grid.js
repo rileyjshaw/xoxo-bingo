440cc735f36d1c6875d95965060d9083e38b6029
Eli Feasley <eliana@khanacademy.org>
2015-09-13 15:40:49 -0700

    Attendee grid generates for users

--- attendee-grid.js
+++ attendee-grid.js
@@ -1,12 +1,104 @@
 // TODO(rjs): stub
-function attendeeGrid (handle) {
-  return [
-    ['test01', 'test02', 'test03', 'test04', 'test05'],
-    ['test11', 'test12', 'test13', 'test14', 'test15'],
-    ['test21', 'test22', handle, 'test24', 'test25'],
-    ['test31', 'test32', 'test33', 'test34', 'test35'],
-    ['test31', 'test32', 'test33', 'test34', 'test35'],
-  ];
-}
+var fs = require('fs');
+var seedrandom = require('seedrandom');
+
+
+// Dimensions of the BINGO card square though not using 5 makes you a
+// MONSTER
+var squareSize = 5;
+var squareCenter = Math.floor(squareSize / 2);
+
+// for xoxo participants to see
+var attendees = JSON.parse(fs.readFileSync('attendees.json', 'utf8'));
+// for everyone else
+var followers = JSON.parse(fs.readFileSync('followers.json', 'utf8'));
+var blacklist = [];
+
+
+var bingoFriendHandles = function(username) {
+    var is_attendee = attendees.indexOf(username) !== -1;
+    var handles = is_attendee ? attendees.slice(0) : followers.slice(0);
+    // remove username from handles
+    return handles.filter(function(i) {
+		return i !== username;
+	});
+};
+
+
+// let folks RSVP 'no' to the party
+var rsvp_no = function(username) {
+    blacklist.push(username);
+};
+
+
+// adapted from underscore
+var shuffle = function(set, rng) {
+    var length = set.length;
+    var shuffled = Array(length);
+    for (var index = 0, rand; index < length; index++) {
+        rand = Math.floor(rng() * index);
+        if (rand !== index) shuffled[index] = shuffled[rand];
+        shuffled[rand] = set[index];
+    }
+    return shuffled;
+};
+
+
+var attendeeListForUsername = function(username) {
+    // we're only showing the xoxo card to xoxo attendees. other folks see
+    // twitter accounts of popular xoxo followers
+    var handles = bingoFriendHandles(username);
+
+    // seed the random number generator so every user always sees the same
+    // card.
+    var rng = seedrandom(username);
+    return shuffle(handles, rng);
+};
+
+
+var arrayIndexFromCoords = function(row, column, handles, handlesSoFar) {
+    var index = squareSize * row + column;
+    var handle = handles[handlesSoFar];
+    if (blacklist.indexOf(handle) == -1) return index;
+
+    var lastGridIndex = squareSize * squareSize;
+    // if the intended bingo person is on the blacklist, remove them from
+    // the grid and fill in someone new.
+    for (var i = lastGridIndex; i < handles.length; i++) {
+    	handle = handles[i];
+    	if (blacklist.indexOf(handle) === -1 && handlesSoFar.indexOf(handle) == -1){
+    		return index;
+    	}
+    }
+};
+
+var userForLocation = function(
+		row, column, handles, username, handlesSoFar) {
+    // Set center avatar to the person viewing
+    if (row == squareCenter && column == squareCenter) {
+        return username;
+    }
+    return handles[arrayIndexFromCoords(row, column, handles, handlesSoFar)];
+};
+
+var attendeeGrid = function(username) {
+    var handles = attendeeListForUsername(username);
+    var card = [];
+    // handles so far is a list of all the handles the user has
+    // seen so far.
+    var bingoRow, handle, handlesSoFar = null;
+    for (var row = 0; row < squareSize; row++) {
+        bingoRow = [];
+        for (var column = 0; column < squareSize; column++) {
+            handle = userForLocation(
+            	row, column, handles, username,
+            	handlesSoFar);
+            bingoRow.push(handle);
+        }
+        card.push(bingoRow);
+    }
+    return card;
+};
+
 
 module.exports = attendeeGrid;
