80e0544941e4e47577abf61f1ec734823a10d1cf
Eli Feasley <eliana@khanacademy.org>
2015-09-20 20:03:38 -0700

    add some cool folks to follow

--- attendee-grid.js
+++ attendee-grid.js
@@ -7,28 +7,23 @@ var seedrandom = require('seedrandom');
 var squareSize = 5;
 var squareCenter = Math.floor(squareSize / 2);
 
-// for xoxo participants to see
-var attendees = JSON.parse(fs.readFileSync('attendees.json', 'utf8'));
-// for everyone else
-var followers = JSON.parse(fs.readFileSync('followers.json', 'utf8'));
-var blacklist = [];
 
+// TODO(eli): Make it so we can read from a database,
+// so that we can show xoxo attendees now that our code is
+// open source
+var followers = JSON.parse(fs.readFileSync('static/public/twitter_lists/folks.json', 'utf8'));
+var blacklist = [];
+// TODO(eli): Grab attendees from a sqlite database
+var attendees = [];
 
 var bingoFriendHandles = function(username) {
     var is_attendee = attendees.indexOf(username) !== -1;
     var handles = is_attendee ? attendees.slice(0) : followers.slice(0);
     // remove username from handles
     return handles.filter(function(i) {
-		return i !== username;
-	});
+        return i !== username;
+    });
 };
-
-
-// let folks RSVP 'no' to the party
-var rsvp_no = function(username) {
-    blacklist.push(username);
-};
-
 
 // adapted from underscore
 var shuffle = function(set, rng) {
@@ -36,7 +31,7 @@ var shuffle = function(set, rng) {
     var shuffled = Array(length);
     for (var index = 0, rand; index < length; index++) {
         rand = Math.floor(rng() * index);
-        if (rand !== index) shuffled[index] = shuffled[rand];
+        if (rand !== index) {shuffled[index] = shuffled[rand];}
         shuffled[rand] = set[index];
     }
     return shuffled;
@@ -44,8 +39,7 @@ var shuffle = function(set, rng) {
 
 
 var attendeeListForUsername = function(username) {
-    // we're only showing the xoxo card to xoxo attendees. other folks see
-    // twitter accounts of popular xoxo followers
+    // ensure we don't show the user herself
     var handles = bingoFriendHandles(username);
 
     // seed the random number generator so every user always sees the same
@@ -58,23 +52,23 @@ var attendeeListForUsername = function(username) {
 var arrayIndexFromCoords = function(row, column, handles, handlesSoFar) {
     var index = squareSize * row + column;
     var handle = handles[handlesSoFar];
-    if (blacklist.indexOf(handle) == -1) return index;
+    if (blacklist.indexOf(handle) === -1) {return index;}
 
     var lastGridIndex = squareSize * squareSize;
     // if the intended bingo person is on the blacklist, remove them from
     // the grid and fill in someone new.
     for (var i = lastGridIndex; i < handles.length; i++) {
-    	handle = handles[i];
-    	if (blacklist.indexOf(handle) === -1 && handlesSoFar.indexOf(handle) == -1){
-    		return index;
-    	}
+        handle = handles[i];
+        if (blacklist.indexOf(handle) === -1 && handlesSoFar.indexOf(handle) === -1) {
+            return index;
+        }
     }
 };
 
 var userForLocation = function(
-		row, column, handles, username, handlesSoFar) {
+    row, column, handles, username, handlesSoFar) {
     // Set center avatar to the person viewing
-    if (row == squareCenter && column == squareCenter) {
+    if (row === squareCenter && column === squareCenter) {
         return username;
     }
     return handles[arrayIndexFromCoords(row, column, handles, handlesSoFar)];
@@ -90,8 +84,8 @@ var attendeeGrid = function(username) {
         bingoRow = [];
         for (var column = 0; column < squareSize; column++) {
             handle = userForLocation(
-            	row, column, handles, username,
-            	handlesSoFar);
+                row, column, handles, username,
+                handlesSoFar);
             bingoRow.push(handle);
         }
         card.push(bingoRow);
